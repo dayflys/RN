728dad23aee3301985a58bae7d20ffd7aa722b25
kkwr0504 <kkwr0504@naver.com>
2022-07-08 15:06:41 +0900

    여기까지

--- App.js
+++ App.js
@@ -1,20 +1,43 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { StyleSheet, Text,Dimensions, View,ScrollView } from 'react-native';
+
+const {width: SCREEN_WIDTH} = Dimensions.get("window");
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={styles.contanier}>
+      <View style={styles.city}>
+        <Text style={styles.cityName}>Seoul</Text>
+      </View>
+      <ScrollView pagingEnabled showsHorizontalScrollIndicator={false} horizontal contentContainerStyle={styles.weather}>
+          <View style={styles.day}>
+            <Text style={styles.temp}>27</Text>
+            <Text style={styles.description}>Sunny</Text>
+          </View>
+          <View style={styles.day}>
+          <Text style={styles.temp}>27</Text>
+          <Text style={styles.description}>Sunny</Text>
+          </View>
+          <View style={styles.day}>
+          <Text style={styles.temp}>27</Text>
+          <Text style={styles.description}>Sunny</Text>
+          </View>
+          <View style={styles.day}>
+          <Text style={styles.temp}>27</Text>
+          <Text style={styles.description}>Sunny</Text>
+          </View>
+      </ScrollView>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+  contanier: {flex:1, backgroundColor:"tomato"},
+  city: {flex: 1.2 , justifyContent:"center", alignItems:"center"},
+  cityName: {fontSize: 68, fontWeight: "500"},
+  weather: {},
+  day: { width: SCREEN_WIDTH, alignItems:"center"},
+  temp:{fontSize: 178, marginTop: 50},
+  description:{fontSize: 60}
+})
+
